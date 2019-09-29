0a45a2d4d080ed7840109bbf4fedadf4b62289c6
Jules <julesauffret@gmail.com>
2019-09-29 15:36:16 +0200

    merge master

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 let a = "a"
 let b = "b"
-<<<<<<< HEAD
-let c = "c"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-let d="d"
-=======
+let d = "d"
 let c = "c2"
->>>>>>> master
-=======
-let d = " d"
->>>>>>> Stashed changes
-=======
-let d = " d"
->>>>>>> Stashed changes
