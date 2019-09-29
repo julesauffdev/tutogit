17c1667ad2868def4bcc1ac9e54baaee239b120c
Jules <julesauffret@gmail.com>
2019-09-29 16:15:42 +0200

    remove conflicts merging

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 let test = "test"
 let newValue = "toto"
-<<<<<<< Updated upstream
-let conf2 = {
-    root: '/linux767',
-=======
 let conf = {
     root: '/linux',
->>>>>>> Stashed changes
     path: '/home/user'
 }
